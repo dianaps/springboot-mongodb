b6b8cd9e9d5c7cce088ef9cd92ebffa16ca60cf2
Diana Alejandra <diannalejandra.ps@gmail.com>
2024-03-05 00:37:42 -0600

    add get hostname

--- src/main/java/com/springboot/demoMongoDB/controller/CustomerController.java
+++ src/main/java/com/springboot/demoMongoDB/controller/CustomerController.java
@@ -2,12 +2,14 @@ package com.springboot.demoMongoDB.controller;
 
 import com.springboot.demoMongoDB.repository.CustomerRepository;
 import com.springboot.demoMongoDB.model.Customer;
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
 import java.util.List;
 
 @RestController
@@ -23,5 +25,13 @@ public class CustomerController {
     @GetMapping("/customers")
     public List<Customer> getAll(){
         return customerRepository.findAll();
+    }
+
+    // Get hostname
+    @SneakyThrows
+    @GetMapping("/getHost")
+    public String getHost(){
+        InetAddress ia = InetAddress.getLocalHost();
+        return  "Hostname: " + ia.getHostName() + " Hostadress: " + ia.getHostAddress();
     }
 }
